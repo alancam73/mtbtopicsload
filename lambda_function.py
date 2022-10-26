95ef6c1fbd161878202ba2c1de25fa85ed9a7657
wildrydes-at-007786019690 <alancam73@yahoo.com>
2022-10-26 03:27:17 +0000

    Turned MTB topics load fxn into a Lambda to run every 24h

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,7 @@
 from __future__ import with_statement
 import boto3
 import json
+import os
 from boto3.dynamodb.conditions import Key, Attr
 from botocore.exceptions import ClientError
 import argparse
@@ -12,14 +13,22 @@ import argparse
 dynamodbtbl=boto3.resource('dynamodb')
 articles_table = dynamodbtbl.Table('mtbTopics-Articles-Topics')
 
-def main():
+# main lambda function handler
+def lambda_handler(event, context):
    
-   parser = argparse.ArgumentParser()
- 
-   # Adding positional arguments
-   parser.add_argument("articlesFile", help = "JSON file with article details")
-   args = parser.parse_args()
-   articlesFile = args.articlesFile
+   # allow local Python execution testing as well as Lambda env
+   execEnv = str(os.getenv('AWS_EXECUTION_ENV'))
+   if execEnv.startswith("AWS_Lambda"):
+      articlesFile = str(os.getenv('articlesFile'))
+   else:
+      parser = argparse.ArgumentParser()
+    
+      # Adding positional arguments
+      parser.add_argument("articlesFile", help = "JSON file with article details")
+      args = parser.parse_args()
+      articlesFile = args.articlesFile
+    
+   print ("Articles File: ", articlesFile)
    
    try: 
       with open(articlesFile, 'r') as myfile:
@@ -41,8 +50,10 @@ def main():
       except ClientError as e:
          if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
              print("put_item: articleId=", articleId, " failed (or duplicate)")
-          
+   
+   return None       
+
 
 # allow local Python execution testing
 if __name__ == '__main__':
-    main()
+    lambda_handler(None,None)
